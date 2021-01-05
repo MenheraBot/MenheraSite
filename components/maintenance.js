6683471650237a9fc266b0d0a8dabd2fbb4f8b23
Luxanna <CommandPlayermine@gmail.com>
2021-01-05 04:07:37 -0300

    :rocket: | Deploy the new Site

--- components/maintenance.js
+++ components/maintenance.js
@@ -7,7 +7,7 @@ export default function Header() {
     <Head title="Manutenção | Menhera's Site" favicon="assets/icon404.png" />
       <img src="assets/sorry.png" />
       <h1>🚧 Está página está indisponível 🚧</h1>
-      <p>Desculpe, mas esta página encontra-se em manutenção atualmente!</p>
+      <p>Desculpe, mas esta página encontra-se em manutenção atualmente :(</p>
       <a href="/"><b>Voltar para o início</b></a>
     </div>
   )
