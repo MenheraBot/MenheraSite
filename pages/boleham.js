96ef76cd552573dcc4a02a137ddc20461e06bba1
Yslan Ramos <yslan3000@gmail.com>
2021-03-03 02:18:12 -0300

    fix footer broken

--- pages/boleham.js
+++ pages/boleham.js
@@ -1,9 +1,10 @@
 import Maintenance from '../components/maintenance'
 import { withTranslation } from '../services/i18n'
+import styles from '../styles/pages/boleham.module.css'
 
-const Boleham = ({ t }) => {
+const Boleham = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Maintenance />
     </div>
   )
