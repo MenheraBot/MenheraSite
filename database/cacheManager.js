6961b0eb552921e8b05d516bf1954b9ba5dedb64
Luxanna <CommandPlayermine@gmail.com>
2021-05-20 22:06:27 -0300

    :alien: | Make commands automatic

---
+++ database/cacheManager.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+class CacheManager {
+  constructor() {
+    this.status = []
+    this.commands = {}
+    this.lastFetch = 0;
+
+    this.fetchDataFromAPI()
+    setInterval(() => {
+      this.fetchDataFromAPI()
+    }, 1000 * 60 * 60)
+  }
+
+  getStatus() {
+    return this.status
+  }
+
+  getCommands(language) {
+    if (language === 'en') return this.commands.en
+    return this.commands.pt
+  }
+
+  fetchDataFromAPI() {
+    const res = await axios.get(process.env.API_URL)
+
+    const ptCommands = []
+    const enCommands = []
+    res.data.commands.forEach(a => {
+      ptCommands.push({ name: a.name, description: a.pt_description, category: a.category })
+      enCommands.push({ name: a.name, description: a.en_description, category: a.category })
+    })
+
+    this.status = res.data.status
+    this.commands = { pt: ptCommands, en: enCommands }
+    this.lastFetch = Date.now()
+  }
+}
+
+const cacheManagerInstance = new CacheManager();
+
+export default cacheManagerInstance
