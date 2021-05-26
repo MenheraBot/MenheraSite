6124cddc8939f88c070a4102f13e8ccd5480c100
Yslan Ramos <yslan3000@gmail.com>
2021-05-26 01:34:21 -0300

    new header

--- components/header.js
+++ components/header.js
@@ -1,32 +1,79 @@
-import style from '../styles/components/header.module.css';
-import { withTranslation } from '../services/i18n'
-import { useState } from 'react';
-import constants from '../database/constants.json'
-import { useRouter } from 'next/router'
+import { withTranslation } from "../services/i18n";
+import { useState } from "react";
+import constants from "../database/constants.json";
+import Image from "next/image";
+import Link from "next/link";
 
-const Header = ({ t, i18n }) => {
-  const router = useRouter()
-  const [lang, setLang] = useState(i18n.language)
+import { HiTranslate } from "react-icons/hi";
+import { RiArrowDownSLine } from "react-icons/ri";
 
-  const changeLang = () => {
-    i18n.changeLanguage(lang === 'en' ? 'pt' : 'en')
-    setLang(lang === 'en' ? 'pt' : 'en')
-  }
+const Header = ({ i18n }) => {
+  const [showLanguages, setShowLanguages] = useState(false);
+
+  const setLang = (lang) => {
+    i18n.changeLanguage(lang);
+  };
 
   return (
-    <header className={style.header}>
-      <a href="/" id={style.home}><img src="assets/logo.png" /></a>
-      <nav>
-        <ul className={style.links}>
-          <li className={router.pathname === '/comandos' ? style.currentPageTitle : ''}><a href="/comandos">{t('commands')}</a></li>
-          {/*  <li className={currentPage === 'boleham' ? style.currentPageTitle : ''}><a href="/boleham">Roleplay</a></li> */}
-          <li className={router.pathname === '/status' ? style.currentPageTitle : ''}><a href="/status">Status</a></li>
+    <header className="w-full h-20 flex items-center justify-center">
+      <div className="py-2 w-11/12 flex flex-row justify-between">
+        <Link href="/">
+          <Image src="/assets/logo.png" width={190} height={35} />
+        </Link>
+        <ul className="flex flex-row gap-4">
+          <Link href="/comandos">
+            <li className="cursor-pointer hover:text-purple-500">Comandos</li>
+          </Link>
+          <Link href="/status">
+            <li className="cursor-pointer hover:text-purple-500">Status</li>
+          </Link>
+          <Link href={constants.github_url}>
+            <li className="cursor-pointer hover:text-purple-500">Github</li>
+          </Link>
+          <Link href={constants.add_bot_url}>
+            <li className="cursor-pointer hover:text-purple-500">Adicionar</li>
+          </Link>
+          <div
+            className="relative cursor-pointer"
+            onClick={() => setShowLanguages(!showLanguages)}
+          >
+            <div className="ml-2 flex flex-row gap-1 justify-center items-center">
+              <HiTranslate size={25} />
+              <RiArrowDownSLine size={15} />
+            </div>
+            {showLanguages && (
+              <div class="absolute bg-white shadow w-28 h-20 mt-2 right-0 rounded-lg">
+                <ul className="flex flex-col gap-2 justify-center p-2">
+                  <li
+                    className="text-gray-600 font-semibold hover:text-purple-500 flex flex-row items-center gap-2"
+                    onClick={() => setLang("pt")}
+                  >
+                    <Image
+                      src="/assets/brazil-flag.svg"
+                      height={20}
+                      width={30}
+                    />{" "}
+                    PT-BR
+                  </li>
+                  <li
+                    className="text-gray-600 font-semibold hover:text-purple-500 flex flex-row items-center gap-2"
+                    onClick={() => setLang("en")}
+                  >
+                    <Image
+                      src="/assets/united-states-flag.svg"
+                      height={20}
+                      width={30}
+                    />
+                    EN
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </ul>
-      </nav>
-      <button id={style.lbtn} onClick={changeLang}>{lang == 'en' ? <img className={style.emoji} src="assets/eua.png" /> : <img className={style.emoji} src="assets/br.png" />}</button>
-      <a id={style.add} href={constants.add_bot_url}><button>{t('add')}</button></a>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default withTranslation('header')(Header)
+export default withTranslation("header")(Header);
