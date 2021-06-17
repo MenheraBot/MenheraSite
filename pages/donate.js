d02dc0a1d67ed2940fa2cd9816b65c82f3cb8c15
Luxanna <CommandPlayermine@gmail.com>
2021-06-17 08:16:39 -0300

    :star: | Better status page

--- pages/donate.js
+++ pages/donate.js
@@ -5,7 +5,7 @@ import styles from '../styles/pages/boleham.module.css'
 const Donate = ({ t }) => {
   return (
     <div>
-      <h1></h1>
+      <h1><a href="https://www.patreon.com/menherabot">BECOME A PATREON</a></h1>
     </div>
   );
 };
