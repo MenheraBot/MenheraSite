e95a5f7795f5346daf056ebee7ba25433bf3dc50
Luxanna <CommandPlayermine@gmail.com>
2021-09-21 01:02:19 -0300

    a

--- next.config.js
+++ next.config.js
@@ -29,5 +29,18 @@ module.exports = {
       destination: add_bot_url,
       permanent: true
     }
-  ]
+  ],
+  headers: async () =>
+    [
+      {
+        source: "*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+
 }
