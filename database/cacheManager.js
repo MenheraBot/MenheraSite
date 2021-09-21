a24c2705a6cd73b508aa9d2bae4914b3428416b2
Luxanna <CommandPlayermine@gmail.com>
2021-09-21 11:29:09 -0300

    cors?

--- database/cacheManager.js
+++ database/cacheManager.js
@@ -18,17 +18,11 @@ class CacheManager {
 
   async fetchDataFromAPI(wannaStatus = false) {
     const res = await axios.get(process.env.API_URL, {
-      withCredentials: false,
-        
+      withCredentials: true,
+
       headers: {
         "Access-Control-Allow-Credentials": true,
-        "Access-Control-Allow-Origin": "*",
-
-        "Access-Control-Allow-Methods":
-          "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-
-        "Access-Control-Allow-Headers":
-          "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+        "Access-Control-Allow-Origin": process.env.API_URL
       }
     })
 
