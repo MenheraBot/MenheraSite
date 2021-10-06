480a01c5015c54835e8f949836746cf0c982c40c
Luxanna <CommandPlayermine@gmail.com>
2021-10-05 23:16:53 -0300

    fix(api): try to fix

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,10 @@
 import axios from 'axios';
 import { Command, Shard } from './api.types';
 
-const fetch = <R>(route: string): (() => Promise<R>) => {
-  return async () => {
-    const res = await axios.get<R>(process.env.API_URL + route);
-    return res.data;
-  };
+const fetch = async <R>(route: string): Promise<R> => {
+  const res = await axios.get<R>(process.env.API_URL + route);
+  return res.data;
 };
 
-export const fetchCommands = fetch<Command[]>('/commands');
-export const fetchStatus = fetch<Shard[]>('/shards');
+export const fetchCommands = (): Promise<Command[]> => fetch('/commands');
+export const fetchStatus = (): Promise<Shard[]> => fetch('/shards');
