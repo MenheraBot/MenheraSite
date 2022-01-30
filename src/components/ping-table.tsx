1314c9744ec8741683f7a6eda252489e81ec7859
Luxanna <CommandPlayermine@gmail.com>
2022-01-30 02:06:06 -0300

    fix: Fix status ping

--- src/components/ping-table.tsx
+++ src/components/ping-table.tsx
@@ -56,7 +56,7 @@ const PingTable = ({ pings }: Props): JSX.Element => {
                 {a.isOff ? (
                   <span className={style.off}>OFF</span>
                 ) : (
-                  moment.utc(Date.now() + a.uptime).format('D[d], H[h], m[m], s[s]')
+                  moment.utc(a.uptime).format('D[d], H[h], m[m], s[s]')
                 )}
               </td>
             </tr>
