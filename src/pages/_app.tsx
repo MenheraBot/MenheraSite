e56ae5075390e662f6b20351dc45d292b9262590
Luxanna <CommandPlayermine@gmail.com>
2022-03-01 14:42:51 -0300

    feat: Add adsense in head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,11 +29,6 @@ const Application = ({ Component, pageProps }: AppProps) => {
     <>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
-        <script
-          async
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_AD_ID}`}
-          crossOrigin='anonymous'
-        ></script>
       </ChakraProvider>
       <Analytics />
     </>
