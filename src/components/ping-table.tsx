a7f2be1c06c9b5eaa170735f2f999ef74102826a
Luxanna <CommandPlayermine@gmail.com>
2022-03-01 14:24:01 -0300

    fix: Duration in shards

--- src/components/ping-table.tsx
+++ src/components/ping-table.tsx
@@ -56,7 +56,7 @@ const PingTable = ({ pings }: Props): JSX.Element => {
                 {a.isOff ? (
                   <span className={style.off}>OFF</span>
                 ) : (
-                  moment.utc(a.uptime).format('D[d], H[h], m[m], s[s]')
+                  moment.duration(a.uptime).format('D[d], H[h], m[m], s[s]')
                 )}
               </td>
             </tr>
