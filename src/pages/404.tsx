bb5a5fc45f5d4e58aa0dd9566ec5b1a98f6d0326
Yslan Ramos <yslanramos.r@gmail.com>
2022-06-25 04:42:15 -0300

    feat: standardize error pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,21 +2,20 @@ import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Header } from '../components/common/Header';
 import { Footer } from '../components/common/Footer';
+import { ErrorView } from '../components/ErrorView';
 
 const NotFoundPage = (): JSX.Element => {
   return (
     <>
       <Header />
-      <main className='mx-auto max-w-7xl p-6 my-10 text-center flex flex-col items-center '>
-        <span className='text-primary text-3xl font-bold md:text-7xl'>404</span>
-        <h1 className='text-white text-3xl font-bold mt-4 mb-6'>
-          Oops! Esta página não está disponível <span className='text-primary'>:(</span>
-        </h1>
-        <p className='text-describe text-xl max-w-2xl'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro assumenda delectus placeat
+      <main className='mx-auto max-w-7xl p-6 text-center flex flex-col items-center '>
+        <ErrorView
+          statusCode={404}
+          title='Oops! Esta página não está disponível'
+          text=' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro assumenda delectus placeat
           animi, eligendi quas nam perspiciatis, facere corporis aliquam magni ullam ad dolor
-          inventore omnis quia maxime distinctio aut!
-        </p>
+          inventore omnis quia maxime distinctio aut!'
+        />
       </main>
       <Footer />
     </>
