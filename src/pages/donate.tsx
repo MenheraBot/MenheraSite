c6e2283b01daa491bcda3351d457e8c131cb586c
Yslan Ramos <yslanramos.r@gmail.com>
2022-06-25 03:42:06 -0300

    feat(donate-page): new design UI

--- src/pages/donate.tsx
+++ src/pages/donate.tsx
@@ -1,57 +1,40 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { Text } from '@chakra-ui/react';
 
-import style from '../styles/pages/donate.module.css';
 import { useTranslation } from 'react-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { GetStaticProps } from 'next';
-import Layout from '../components/ui/layout';
+import { Header } from '../components/common/Header';
+import { Footer } from '../components/common/Footer';
+import { SectionDivider } from '../components/common/SectionDivider';
 
 const DonatePage = (): JSX.Element => {
   const { t } = useTranslation('donate');
 
   return (
-    <Layout title={t('title')}>
-      <div className='flex'>
-        <div className='flex w-1/3 text-center'>
-          <Image alt='Menhera Crying' src='/assets/cry.png' width={300} height={300} className='' />
-        </div>
-        <div className='flex-1 text-center'>
-          <Text className='text-7xl text-purple-800 sm:text-6xl font-bold text-shadow'>
-            {t('h1')}
-          </Text>
-          <p className='text-purple-700 text-2xl sm:text-lg max-w-2xl mt-4'>{t('p')}</p>
-          <div className='my-8 text-5xl sm:text-2xl text-white'>
-            <span className='font-semibold text-green-400'>Pix: </span>
-            donate@menherabot.xyz
+    <>
+      <Header />
+      <main className='container p-6 mx-auto max-w-7xl  mb-10'>
+        <SectionDivider title='Donate' withoutSpace className='mb-10' />
+        <div className='lg:flex justify-around items-center'>
+          <div>
+            <Image alt='Menhera Crying' src='/assets/cry.png' width={300} height={300} />
           </div>
-          <Text className='font-semibold text-green-400 text-5xl pb-2'>
-            <Link href={'https://github.com/sponsors/MenheraBot'}>GitHub Sponsors</Link>
-          </Text>
-        </div>
-      </div>
-
-      {/*  <section className='container mx-auto flex self-center items-center place-self-center justify-center text-center h-screen sm:mt-24'>
-          <div className='flex flex-row'>
-            <div className='flex flex-col w-3/4 mt-32'>
-              <h1 className='text-7xl text-purple-800 sm:text-6xl font-bold -mt-52 text-shadow'>
-                {t('h1')}
-              </h1>
-              <p className='text-purple-700 text-2xl sm:text-lg mt-4'>{t('p')}</p>
-
-              <div className='my-8 text-5xl sm:text-2xl text-white'>
-                <span className='font-semibold text-green-400'>Pix: </span>
-                donate@menherabot.xyz
-              </div>
-
-              <div className='w-2/'>
-                <Image alt='Menhera Crying' src='/assets/cry.png' width='300' height='400' />
-              </div>
+          <div>
+            <h1 className='text-white font-bold text-3xl md:text-4x'>{t('h1')}</h1>
+            <p className='text-describe font-describe mt-4 md:text-xl max-w-lg'>{t('p')}</p>
+            <div className='text-primary my-6'>
+              <span className='font-semibold text-green-400'>Pix: </span>
+              donate@menherabot.xyz
             </div>
+            <Link href={'https://github.com/sponsors/MenheraBot'} passHref>
+              <a className='font-semibold text-green-400 text-5xl'>GitHub Sponsors</a>
+            </Link>
           </div>
-        </section> */}
-    </Layout>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 };
 
