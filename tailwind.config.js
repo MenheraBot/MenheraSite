186a0b233fbecbae5e1b506817331a597d76be42
Yslan Ramos <yslanramos.r@gmail.com>
2022-06-26 23:56:17 -0300

    fix: remove tailwind warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   variants: {
     extend: {
       width: ['focus'],
