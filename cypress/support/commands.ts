75216cfa28235909ac8edf6a5a1f6c632f185dbe
Yslan Ramos <yslanramos.r@gmail.com>
2022-06-26 23:26:56 -0300

    feat: setup cypress

---
+++ cypress/support/commands.ts
@@ -0,0 +1,20 @@
+// load type definitions that come with Cypress module
+
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Custom command to get element by data-cy values
+     * @example cy.getByDataTest('selector')
+     */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    getByDataTest(selector: string, ...args: any[]): Chainable<Window>;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+Cypress.Commands.add('getByDataTest', (selector: string, ...args) => {
+  return cy.get(`[data-test="${selector}"]`, ...args);
+});
