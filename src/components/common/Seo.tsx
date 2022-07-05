043b04c060c5c8b954b802e301e0c03014aefa98
Luxanna <CommandPlayermine@gmail.com>
2022-07-05 20:11:04 -0300

    feat: improve seo

--- src/components/common/Seo.tsx
+++ src/components/common/Seo.tsx
@@ -20,27 +20,44 @@ export function Seo({ page }: Props): JSX.Element {
     <Head>
       <title>{t('title')}</title>
       <meta name='description' content={t('description')} />
+      <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
 
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta httpEquiv='Content-Type' content='text/html;charset=UTF-8' />
+      <meta name='robots' content='index, follow' />
+      <meta name='googlebot' content='index, follow' />
+      <link rel='canonical' href={canonicalUrl} />
       <meta charSet='utf-8' />
+      <meta
+        name='keywords'
+        content='menhera, bot, discord, discordbot, menherabot, menhera bot, bot brasileiro'
+      />
+      <meta name='theme-color' content='#975AFF' />
+
+      <link rel='alternate' href='http://menherabot.xyz/' hrefLang='x-default' />
       <link rel='alternate' href='http://menherabot.xyz/en-US' hrefLang='en-US' />
       <link rel='alternate' href='http://menherabot.xyz/pt-BR' hrefLang='pt-BR' />
       <link rel='alternate' href='http://menherabot.xyz/en-US' hrefLang='en' />
       <link rel='alternate' href='http://menherabot.xyz/pt-BR' hrefLang='pt' />
-      <link rel='canonical' href={canonicalUrl} />
       <meta name='language' content={i18n.language} />
       <meta httpEquiv='content-language' content={i18n.language} />
 
       <meta property='og:locale' content={i18n.language} />
+      <meta property='og:locale:alternate' content='pt-BR' />
+      <meta property='og:locale:alternate' content='en-US' />
       <meta property='og:title' content={t('title')} />
       <meta property='og:type' content='website' />
       <meta property='og:url' content='https://menherabot.xyz/' />
       <meta property='og:image' content='https://menherabot.xyz/images/menhera-bot.png' />
+      <meta
+        property='og:image:secure_url'
+        content='https://menherabot.xyz/images/menhera-bot.png'
+      />
       <meta property='og:image:type' content='image/png' />
+      <meta property='og:site_name' content='MenheraBot' />
       <meta property='og:image:width' content='1024' />
       <meta property='og:image:height' content='1024' />
       <meta property='og:description' content={t('description')} />
-      <meta name='theme-color' content='#975AFF' />
     </Head>
   );
 }
