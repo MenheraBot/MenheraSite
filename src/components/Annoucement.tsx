f1c58043d4a0acb3ce49e099f66922d8fb63a48b
Luan Caliel <luxanna@menherabot.xyz>
2022-09-11 15:04:17 -0300

    Update Annoucement.tsx

--- src/components/Annoucement.tsx
+++ src/components/Annoucement.tsx
@@ -10,8 +10,8 @@ export const Announcement = (): JSX.Element => {
               <HiSpeakerphone className='h-6 w-6 text-white' aria-hidden='true' />
             </span>
             <p className='ml-3 font-medium text-white truncate'>
-              <span className='md:hidden'>Manutenção Longa</span>
-              <span className='hidden md:inline'>A Menhera está offline para uma manutenção</span>
+              <span className='md:hidden'>Inoperabilidade</span>
+              <span className='hidden md:inline'>A Menhera está offline! Entre no servidor do Discord para mais informações</span>
             </p>
           </div>
           <div className='order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto'>
