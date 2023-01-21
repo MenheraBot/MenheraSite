d8851a4e63e97c9600e3f7d481061d86d9a297f1
ySnoopyDogy <CommandPlayermine@gmail.com>
2023-01-21 04:45:40 -0300

    feat: add cookie consent
    closes #67
    fixes #78

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,6 +1,8 @@
 import { Footer } from './common/Footer';
 import { Seo } from './common/Seo';
 import { Header } from './common/Header';
+import CookieConsent from 'react-cookie-consent';
+import { useTranslation } from 'next-i18next';
 
 type Props = {
   children: React.ReactNode;
@@ -8,13 +10,32 @@ type Props = {
 };
 
 const Layout = ({ children, page }: Props): JSX.Element => {
+  const { t } = useTranslation('common');
+
   return (
-    <>
+    <main>
       <Seo page={page} />
       <Header />
+      {/* @ts-expect-error This works as intended  */}
+      <CookieConsent
+        location='bottom'
+        buttonText='Okay'
+        style={{ background: '#000' }}
+        buttonStyle={{
+          color: '#fff',
+          fontWeight: 'bold',
+          fontSize: '15px',
+          background: '#9c5ddb',
+          borderRadius: '50px',
+          width: '300px',
+          heigh: '30px',
+        }}
+      >
+        {t('cookie')}
+      </CookieConsent>
       {children}
       <Footer />
-    </>
+    </main>
   );
 };
 
